be5ad2fdc9d20a2bebfa5570aabe92be7b3df1b8
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-02-06 10:32:01 -0300

    03.01_Criando_Arquivos_Python

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print(("-" *3) + " Agenda de contatos TW " + ("-" *3))
 print("-" * 30)
 
 opcao_menu = 1
-lista_contatos = list()
+#lista_contatos = list()
 
 while(opcao_menu != 0):
     print("1. Listar contatos")
@@ -15,36 +15,37 @@ while(opcao_menu != 0):
     print("0. Sair")
     opcao_menu = int(input("Digite a opção desejada: "))
 
-    if opcao_menu == 1:
-        for i in lista_contatos:
-            print(f"nome: {i.nome} / email: {i.email} / telefone: {i.telefone}")
-    elif opcao_menu == 2:
+    # if opcao_menu == 1:
+    #     for i in lista_contatos:
+    #         print(f"nome: {i.nome} / email: {i.email} / telefone: {i.telefone}")
+    if opcao_menu == 2:
+        arquivo = open("contatos.txt", "a")
         nome_contato = input("Digite o nome do contato: ")
         email_contato = input("Digite o email do contato: ")
         telefone_contato = input("Digite o telefone do contato: ")
         contato_novo = Contato(nome_contato, email_contato, telefone_contato)
-        lista_contatos.append(contato_novo)
-    elif opcao_menu == 3:
-        contato_remover = input("Digite o email do contato que deseja remover: ")
-        contato_encontrado = False
-        for i in lista_contatos:
-            if i.email == contato_remover:
-                lista_contatos.remove(i)
-                print("Contato removido")
-                contato_encontrado = True
-                continue
-        if not contato_encontrado:
-            print("Contato não encontrado")
-    elif opcao_menu == 4:
-        contato_buscar = input("Digite o email do contato que deseja buscar: ")
-        contato_encontrado = False
-        for i in lista_contatos:
-            if i.email == contato_buscar:
-                print(f"nome: {i.nome} / email: {i.email} / telefone: {i.telefone}")
-                contato_encontrado = True
-                continue
-        if not contato_encontrado:
-            print("Contato não encontrado")
+        #lista_contatos.append(contato_novo)
+    # elif opcao_menu == 3:
+    #     contato_remover = input("Digite o email do contato que deseja remover: ")
+    #     contato_encontrado = False
+    #     for i in lista_contatos:
+    #         if i.email == contato_remover:
+    #             lista_contatos.remove(i)
+    #             print("Contato removido")
+    #             contato_encontrado = True
+    #             break
+    #     if not contato_encontrado:
+    #         print("Contato não encontrado")
+    # elif opcao_menu == 4:
+    #     contato_buscar = input("Digite o email do contato que deseja buscar: ")
+    #     contato_encontrado = False
+    #     for i in lista_contatos:
+    #         if i.email == contato_buscar:
+    #             print(f"nome: {i.nome} / email: {i.email} / telefone: {i.telefone}")
+    #             contato_encontrado = True
+    #             break
+    #     if not contato_encontrado:
+    #         print("Contato não encontrado")
 
     else:
         print("opcao invalida")
